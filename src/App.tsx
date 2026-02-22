893ddc51fa904777d65a9d616ce718024286e48c
Felipe Maia <felipe_356@hotmail.com>
2026-02-22 12:02:32 -0300

    hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import LoginCliente from "./pages/LoginCliente";
@@ -26,7 +26,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <PageLoadingIndicator />
         <Routes>
           <Route path="/" element={<Index />} />
@@ -44,7 +44,7 @@ const App = () => (
           <Route path="/cliente/casos/:id" element={<DetalhesProcessoCliente />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
