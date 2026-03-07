77ca30c6c0a0eef2de696f99bbce0d7449b06b50
Felipe Maia <felipe_356@hotmail.com>
2026-03-07 00:56:10 -0300

    fix: builds

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/jusclient/',
+  base: mode === "ghpages" ? "/jusclient/" : "/",
   server: {
     host: "::",
     port: 8080,
